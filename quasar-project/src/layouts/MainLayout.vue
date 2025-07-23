275019db5df7f997f6143088123c3a9aba116dd9
Rayane <r.kismoun@it-students.fr>
2025-07-23 16:46:42 +0200

    Fix config

--- quasar-project/src/layouts/MainLayout.vue
+++ quasar-project/src/layouts/MainLayout.vue
@@ -6,7 +6,7 @@
 
         <q-toolbar-title> Quasar App </q-toolbar-title>
 
-        <!-- <div>Quasar v{{ $q.version }}</div> -->
+        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
