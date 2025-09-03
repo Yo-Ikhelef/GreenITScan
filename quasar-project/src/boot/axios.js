d205c31eedcb02673f1df9991bb55d116160b6f8
Yo-Ikhelef <yo.ikhelef@it-students.fr>
2025-09-03 15:18:32 +0200

    fix api url in axios for both env

--- quasar-project/src/boot/axios.js
+++ quasar-project/src/boot/axios.js
@@ -5,7 +5,6 @@ import { useAuthStore } from '../stores/authStore.js'
 import { watch } from 'vue';
 
 
-
 // Be careful when using SSR for cross-request state pollution
 // due to creating a Singleton instance here;
 // If any client changes this (global) instance, it might be a
@@ -13,13 +12,13 @@ import { watch } from 'vue';
 // "export default () => {}" function below (which runs individually
 // for each client)
 
-const baseURL = (window.__ENV__ && window.__ENV__.API_URL) || import.meta.env.VITE_API_URL || '/api';
-
-
-const api = axios.create({ baseURL });
-
-// const api = axios.create({ baseURL: 'http://localhost:8000/api' });
-
+let api;
+if ((window.__ENV__ && window.__ENV__.API_URL) ) {
+  const baseURL = (window.__ENV__ && window.__ENV__.API_URL) ;
+  api = axios.create({ baseURL });
+} else {
+  api = axios.create({ baseURL: 'http://localhost:8000/api' });
+}
 
 export default defineBoot(({ app }) => {
   const authStore = useAuthStore();
